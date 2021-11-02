380aba105bcd9eb75bdd47c88624d625b6aa3f7a
Grahame Grieve <grahameg@gmail.com>
2021-11-03 05:56:37 +1100

    clean up code

--- source/idetester_direct.pas
+++ source/idetester_direct.pas
@@ -1,5 +1,33 @@
 unit idetester_direct;
 
+{
+Copyright (c) 2011+, Health Intersections Pty Ltd (http://www.healthintersections.com.au)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+ * Neither the name of HL7 nor the names of its contributors may be used to
+   endorse or promote products derived from this software without specific
+   prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+}
+
 {$MODE DELPHI}
 
 interface
@@ -95,7 +123,7 @@ function TTestEngineDirectListener.findNode(test : TTest; optional : boolean) : 
 begin
   result := findTestInNode(test, FRoot);
   if (result = nil) and not optional then
-    raise Exception.create(Format(rs_IdeTester_Err_Node_Not_Found, [test.TestName])); // this really shouldn't happen
+    raise EIDETester.create(Format(rs_IdeTester_Err_Node_Not_Found, [test.TestName])); // this really shouldn't happen
 end;
 
 function TTestEngineDirectListener.makeError(err : TTestFailure) : TTestError;
@@ -283,7 +311,7 @@ end;
 
 procedure TTestEngineDirect.terminateTests(session: TTestSession);
 begin
-  raise Exception.create(rs_IdeTester_Msg_NOT_SUPPORTED);
+  raise EIDETester.create(rs_IdeTester_Msg_NOT_SUPPORTED);
 end;
 
 procedure TTestEngineDirect.finishTestRun(session: TTestSession);
